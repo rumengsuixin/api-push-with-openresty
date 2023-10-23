9ab425c28961de48dc3bb9e4590d6242fc45d828
jong-un <36259838+zwong91@users.noreply.github.com>
2023-10-23 06:52:44 +0000

    yes!

--- backend4/hello.py
+++ backend4/hello.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-import mysql.connector
 
 server = Flask(__name__)
 
@@ -8,19 +7,6 @@ server = Flask(__name__)
 def listBlog():
 
     return '<div>   backend4  ' + '</div>'
-    """
-    """
-    global conn
-    if not conn:
-        conn = DBManager(password_file='/run/secrets/db-password')
-        conn.populate_db()
-    rec = conn.query_titles()
-
-    response = 'backend4'
-    for c in rec:
-        response = response  + '<div>   backend4  ' + c + '</div>'
-    return response
-
 
 if __name__ == '__main__':
     server.run()
